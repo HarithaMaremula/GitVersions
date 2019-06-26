e069f753e7b4955f774c7de083ae97a0729de1d9
HarithaMaremula <harithamaremula79@gmail.com>
2019-06-26 14:53:03 +0530

    Initial upload of sql connection to git

---
+++ ConnectionToSql/src/ConnectionToSql.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class ConnectionToSql {
+	public static void main(String args[])
+	{
+		try{  
+		Class.forName("com.mysql.cj.jdbc.Driver");  
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Haritha","root","Root123@");  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from employee");  
+		while(rs.next())  
+		System.out.println(rs.getInt(1)+"  "+rs.getInt(2));  
+		con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+	}
+
+}
+
